e3163858e4f0ba863c8a8c8bb7a2eebb3b3a98fd
johncrimmins <john.crimmins@protonmail.com>
2021-02-13 01:50:51 -0500

    Initial progress on meditation app. Site structure, styling, and videos/sounds in place.

--- app.js
+++ app.js
@@ -1,82 +1,66 @@
-const song = document.querySelector(".song");
-const play = document.querySelector(".play");
-const replay = document.querySelector(".replay");
-const outline = document.querySelector(".moving-outline circle");
-const video = document.querySelector(".vid-container video");
-//Sounds
-const sounds = document.querySelectorAll(".sound-picker button");
-//Time Display
-const timeDisplay = document.querySelector(".time-display");
-const outlineLength = outline.getTotalLength();
-//Duration
-const timeSelect = document.querySelectorAll(".time-select button");
-let fakeDuration = 600;
+const app = () => {
+    /* this is a simlpe arrow function ... what's that mean? */
 
-outline.style.strokeDashoffset = outlineLength;
-outline.style.strokeDasharray = outlineLength;
-timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
-  fakeDuration % 60
-)}`;
+    /* what are things we need? we need the song */
 
-sounds.forEach(sound => {
-  sound.addEventListener("click", function() {
-    song.src = this.getAttribute("data-sound");
-    video.src = this.getAttribute("data-video");
-    checkPlaying(song);
-  });
-});
+    const song = document.querySelector('.song');
 
-play.addEventListener("click", function() {
-  checkPlaying(song);
-});
+    /* need play button */
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle'); /* we want the CIRCLE inside the svg, bc we want to animate the path inside the circle, not the svg */
+    const video = document.querySelector('.vid-container video');
 
-replay.addEventListener("click", function() {
-    restartSong(song);
+    //Sounds
+    const sounds = document.querySelectorAll('.sound-picker button') /*make sure to do .querySelectorAll bc we want alll the sounds*/
+
+    //Time Display - remember this is just the h3
+    const timeDisplay = document.querySelector('.time-display')
+
+    //We need to get the length of the outline of the svg
+    //get length of the outline
+    const outlineLength = outline.getTotalLength();
+
+    //Duration
+    let fakeDuration = 600;
+
+    // technically this should be put into a function, not left free here 
+    outline.style.strokeDasharray = outlineLength; /* this leaves (value) amount of px empty along the path. outlineLenght is current setting, try 100px */
+    outline.style.strokeDashoffset = outlineLength;
+
+    //play sound
+    play.addEventListener("click",() => {
+        CheckPlaying(song);
+    });
+
+    // Create a function to stop and play the sounds
+    const CheckPlaying = song => {
+        if(song.paused) {
+            song.play();
+            video.play();
+            play.src = './svg/pause.svg';
+        } else {
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg';
+        }
+    };
+
+
+    // track time
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60); /* math.floor() provides a nice round integer */
+        let minutes = Math.floor(elapsed / 60);
     
-  });
+    // animate the circle
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress; 
 
-
-const restartSong = song =>{
-    let currentTime = song.currentTime;
-    song.currentTime = 0;
-    console.log("ciao")
-
-}
-
-timeSelect.forEach(option => {
-  option.addEventListener("click", function() {
-    fakeDuration = this.getAttribute("data-time");
-    timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
-      fakeDuration % 60
-    )}`;
-  });
-});
-
-const checkPlaying = song => {
-  if (song.paused) {
-    song.play();
-    video.play();
-    play.src = "./svg/pause.svg";
-  } else {
-    song.pause();
-    video.pause();
-    play.src = "./svg/play.svg";
-  }
+    
+    // animate the text 
+        timeDisplay.textContent = '${minutes}:${seconds}';
+    }
 };
 
-song.ontimeupdate = function() {
-  let currentTime = song.currentTime;
-  let elapsed = fakeDuration - currentTime;
-  let seconds = Math.floor(elapsed % 60);
-  let minutes = Math.floor(elapsed / 60);
-  timeDisplay.textContent = `${minutes}:${seconds}`;
-  let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
-  outline.style.strokeDashoffset = progress;
-
-  if (currentTime >= fakeDuration) {
-    song.pause();
-    song.currentTime = 0;
-    play.src = "./svg/play.svg";
-    video.pause();
-  }
-};
+app();
